336a1be258218bcd3be955269cf248c15770dd68
RolandoRp <rolyrp@hotmail.com>
2023-06-05 16:32:57 -0600

    HW1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,9 +2,23 @@
 
 function BinarioADecimal(num) {
   // escriben aca el codigo
+  var resultado = 0
+  var posicion = num.length - 1
+  for (let i = 0; i < num.length; i++) {
+    resultado = resultado + num[i] * Math.pow(2, posicion)
+    posicion--
+  } return resultado
 }
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+  var string = ''
+  var numero = num
+
+  while (numero > 0) {
+    string = numero %2 + string
+    numero = Math.floor(numero/2)
+  } return string
+}
 
 module.exports = {
   BinarioADecimal,
