87459ab2d3895639a483f7f96fb06e09ba6ce742
RolandoRp <rolyrp@hotmail.com>
2023-06-12 21:30:22 -0600

    hw6

--- 06-EstructuraDeDatos-III/homework/Apuntes.js
+++ 06-EstructuraDeDatos-III/homework/Apuntes.js
@@ -1,5 +1,43 @@
 /* 
 ARBOLES
+*/
+function BinarySearchTree(value) {
+    this.root = value
+    this.left = null
+    this.right = null
+}
 
+BinarySearchTree.prototype.insert = function(value) {
+    if(value < this.root) {
+            //Es menor >>> va a la izquierda(left)
+        if(this.left) { //Solo entro y hago recursion si la rama izquierda esta ocupada
+            //Tengo algo en la rama izquierda
+
+            this.left.insert(value)
+        }
+        else { //No tengo nada en la rama izquierda
+            this.left = new BinarySearchTree(value)
+        }
+    }
+
+    else if(value > this.root) {
+        //Es mayor >>> va a la derecha(right)
+        if(this.right) {
+            this.right.insert(value)
+        }
+        else {
+            this.right = new BinarySearchTree(value)
+        }
+    }
+}
+
+const abrolito = new BinarySearchTree(5)
+
+
+/*
 RECORRIDOS
+Breadth First Search >> Explora los nodos mas cercanos al nodo raiz y luego se expande a nodos cada vez mas lejanos en niveles sucesivos
+Depth First Search Inorder >> Explora los nodos del subarbol izquierdo, luego el nodo actual y finalmente los nodos del subarbol derecho.
+Depth First Search Preorder >> Explora el nodo actual, luego los nodos del subarbol izquierdo y finalmente los nodos del subarbol derecho.
+Depth First Search Postorder >> Explora los nodos del subarbol izquierdo, luego los nodos del subarbol derecho y finalmente el nodo actual.
 */
--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -10,28 +10,103 @@
   - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
-function BinarySearchTree() {
-
+function BinarySearchTree(value) {
+  this.value = value
+  this.left = null
+  this.right = null
 }
 
 BinarySearchTree.prototype.size = function() {
+  if(this.right === null && this.left === null) return 1;
+  if(this.left !== null && this.right === null) return 1 + this.left.size();
+  if(this.left === null && this.right !== null) return 1 + this.right.size();
+  if(this.left !== null && this.right !== null) return 1 + this.left.size() + this.right.size();
 
+  // let counter = 1
+
+  // if(this.left) counter += this.left.size()
+  // if(this.right) counter += this.right.size()
+  // return counter
 }
 
-BinarySearchTree.prototype.insert = function() {
-   
+BinarySearchTree.prototype.insert = function(value) {
+       if(value < this.value) {
+       //Es menor >>> va a la izquierda(left)
+     if(this.left) { //Solo entro y hago recursion si la rama izquierda esta ocupada
+       //Tengo algo en la rama izquierda
+         this.left.insert(value)
+     }
+
+     else { //No tengo nada en la rama izquierda
+       this.left = new BinarySearchTree(value)
+     }
+   }
+
+     else {
+       //Es mayor >>> va a la derecha(right)
+     if(this.right) {
+         this.right.insert(value)
+     }
+     else {
+         this.right = new BinarySearchTree(value)
+     }
+ }
 }
 
-BinarySearchTree.prototype.contains = function() {
-   
+BinarySearchTree.prototype.contains = function(value) {
+   if(this.value === value) return true
+
+   if(value > this.value){
+    //Si entro aca va a la derecha porque es mayor
+    if(this.right === null) return false
+    return this.right.contains(value)
+   }
+
+   if(value < this.value){
+    //Si entro aca va a la izquierda porque es menor
+    if(this.left === null) return false
+    return this.left.contains(value)
+   }
 }
 
-BinarySearchTree.prototype.depthFirstForEach = function() {
-   
+BinarySearchTree.prototype.depthFirstForEach = function(cb, pedido) {
+   //pedido >>> 'in-order' => left, root, right
+   //pedido >>> 'pre-order' =>
+   //pedido >>> 'post-order' =>
+
+   if(pedido === 'in-order' || pedido === undefined) {
+    if(this.left) this.left.depthFirstForEach(cb, pedido);
+    cb(this.value);
+    if(this.right) this.right.depthFirstForEach(cb, pedido);
+   }
+
+   if(pedido === 'pre-order') {
+    cb(this.value);
+    if(this.left) this.left.depthFirstForEach(cb, pedido);
+    if(this.right) this.right.depthFirstForEach(cb, pedido);
+   }
+
+   if(pedido === 'post-order') {
+    if(this.left) this.left.depthFirstForEach(cb, pedido);
+    if(this.right) this.right.depthFirstForEach(cb, pedido);
+    cb(this.value);
+   }
 }
 
-BinarySearchTree.prototype.breadthFirstForEach = function() {
-   
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, value = []) {
+   if(this.left !== null){
+    value.push(this.left)
+   }
+
+   if(this.right !== null){
+    value.push(this.right)
+   }
+
+   cb(this.value)
+
+   if(value.length > 0){
+    value.shift().breadthFirstForEach(cb, value)
+   }
 }
 // No modifiquen nada debajo de esta linea
 // --------------------------------
