a33f14c72a2ebe5e675a841321d06f4180afd1af
RolandoRp <rolyrp@hotmail.com>
2023-06-12 21:31:11 -0600

    hw05

---
+++ 05-EstructuraDeDatos-II/homework/listas homework.js
@@ -0,0 +1,168 @@
+"use strict";
+
+/* EJERCICIO 1
+Implementar la clase LinkedList, definiendo los siguientes métodos:
+  - add: agrega un nuevo nodo al final de la lista;
+  - remove: elimina el último nodo de la lista y retorna su valor (tener en cuenta el caso particular de una lista de un solo nodo y de una lista vacía);
+  - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
+  EJEMPLO 
+  search(3) busca un nodo cuyo valor sea 3;
+  search(isEven), donde isEven es una función que retorna true cuando recibe por parámetro un número par, busca un nodo cuyo valor sea un número par.
+  En caso de que la búsqueda no arroje resultados, search debe retornar null.
+*/
+function LinkedList() {
+  this.head = null;
+  this._length = 0;
+}
+
+function Node(value) {
+  (this.value = value), (this.next = null);
+}
+
+LinkedList.prototype.add = function (value) {
+  let nuevoNodo = new Node(value);
+  // Si no existe this.head
+  if (!this.head) {
+    this.head = nuevoNodo;
+  } else {
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = nuevoNodo;
+  }
+
+  this._length++;
+  return nuevoNodo;
+};
+LinkedList.prototype.remove = function () {
+  // Vacia
+  if (this._length === 0) return null;
+  // if(!this.head) return null
+  // 1 solo nodo
+  // else if(this._length === 1)
+  else if (this.head.next === null) {
+    let aux = this.head.value;
+    this.head = null;
+    this._length--;
+    return aux;
+  }
+
+  let current = this.head;
+  while (current.next.next) {
+    current = current.next;
+  }
+
+  let aux = current.next.value;
+  current.next = null;
+  this._length--;
+  return aux;
+};
+LinkedList.prototype.search = function (param) {
+  // param --> 10 -> PARA DARNOS CUENTA SI ENCONTRAMOS -> current.value === param
+  // param --> funcion ->  PARA DARNOS CUENTA SI ENCONTRAMOS-> let booleano = param(current.value) -> true/false
+
+  if (!this.head) return null;
+  let current = this.head;
+  while (current) {
+    // Valor
+    if (current.value === param) {
+      // lo encontré
+      return current.value;
+    } else if (typeof param === "function") {
+      // Funcion
+      // let booleano = param(current.value) // true/false
+      if (param(current.value)) {
+        return current.value;
+      }
+    }
+    current = current.next;
+  }
+  // Recorrí la lista sin resultados
+  return null;
+};
+
+const lista = new LinkedList(); // {head: null}
+
+/* EJERCICIO 2
+Implementar la clase HashTable.
+Nuetra tabla hash, internamente, consta de un arreglo de buckets (slots, contenedores, o casilleros; es decir, posiciones posibles para almacenar la información), donde guardaremos datos en formato clave-valor (por ejemplo, {instructora: 'Ani'}).
+Para este ejercicio, la tabla debe tener 35 buckets (numBuckets = 35). (Luego de haber pasado todos los tests, a modo de ejercicio adicional, pueden modificar un poco la clase para que reciba la cantidad de buckets por parámetro al momento de ser instanciada.)
+
+La clase debe tener los siguientes métodos:
+  - hash: función hasheadora que determina en qué bucket se almacenará un dato. Recibe un input alfabético, suma el código numérico de cada caracter del input (investigar el método charCodeAt de los strings) y calcula el módulo de ese número total por la cantidad de buckets; de esta manera determina la posición de la tabla en la que se almacenará el dato
+  - set: recibe el conjunto clave valor (como dos parámetros distintos), hashea la clave invocando al método hash, y almacena todo el conjunto en el bucket correcto.
+  - get: recibe una clave por parámetro, y busca el valor que le corresponde en el bucket correcto de la tabla.
+  - hasKey: recibe una clave por parámetro y consulta si ya hay algo almacenado en la tabla con esa clave (retorna un booleano).
+
+Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
+*/
+
+function HashTable() {
+  this.buckets = [];
+  this.numBuckets = 35;
+}
+
+HashTable.prototype.hash = function (key) {
+  // key --> "instructora"
+  //               i
+  let sum = 0;
+  for (let i = 0; i < key.length; i++) {
+    sum += key.charCodeAt(i);
+  }
+  return sum % this.numBuckets;
+};
+
+HashTable.prototype.set = function (key, value) {
+  if (typeof key !== "string") throw TypeError("Keys must be strings");
+  let posicion = this.hash(key); // instructora --> 24
+
+  // Si esta vacia la posicion, guardo un obj para evitar futuras colisiones
+  if (this.buckets[posicion] === undefined) {
+    this.buckets[posicion] = {};
+  }
+
+  // Validar que no exista una key igual, asi no guardo keys repetidas
+
+  // if (this.hasKey(key)) {
+  //   return null;
+  // } else {
+  //   this.buckets[posicion][key] = value;
+  // }
+
+  this.buckets[posicion][key] = value;
+
+  // [mas cosas, {instructora: {1: Ani, 2: Dai}},{},{},{},{}]
+};
+
+HashTable.prototype.get = function (key) {
+  let posicion = this.hash(key); // 24
+  let valor = this.buckets[posicion][key]; // [{instructora: "Dai"}]
+  return valor;
+};
+
+HashTable.prototype.hasKey = function (key) {
+  let posicion = this.hash(key);
+  return this.buckets[posicion].hasOwnProperty(key); // true/false
+};
+
+const tabla = new HashTable();
+tabla.set("instructora", "Dai");
+tabla.get("instructora");
+tabla.hasKey("instructora");
+
+// HENRY --> 2
+// HENRY --> 2
+// ARRAY --> 2
+// JAVASCRIPT --> 5
+
+// No modifiquen nada debajo de esta linea
+// --------------------------------
+
+module.exports = {
+  Node,
+  LinkedList,
+  HashTable,
+};
